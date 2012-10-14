a8f922e4ab7f060666653039aa847096ae1247a0
Matt Steele <orphum@gmail.com>
2012-10-14 11:28:07 -0500

    Add jquery plugin and first test

--- specs/catslock-bookmarklet-spec.js
+++ specs/catslock-bookmarklet-spec.js
@@ -1,4 +1,14 @@
 describe("Cats Lock Bookmarklet", function() {
-  it("does something", function() {
+  it("is a jquery plugin", function() {
+    expect(typeof(jQuery.fn.catsLock)).toEqual('function');
   });
+
+  xit("runs on an image");
+
+  xit("runs on all images below selected element");
+
+  xit("can show placedog");
+  xit("has placesheen");
+
+  xit("can use other placeholder formats");
 });
